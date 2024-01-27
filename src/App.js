f90e70d7e68a7e6f2c889348bcc880018e27c44f
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-01-27 14:23:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+ import './App.css';
+
+import CartButton from './Components/Cart/CartCount';
+function App() {
+  return (
+    <div className="App">
+   
+      <CartButton />
+     
+      
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Cart/CartCount.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CartCount.css'
+
+const CartButton = () => {
+  return (
+    <div>
+      <nav>
+        <h1>ReactMeals</h1>
+    
+          <h4>Your cart</h4>
+      
+      </nav>
+    </div>
+  );
+};
+
+export default CartButton;
