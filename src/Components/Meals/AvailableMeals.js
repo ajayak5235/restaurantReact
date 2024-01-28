0549f1ccf376d3611df820d3ec9b878b63ac0d20
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-01-28 20:42:43 +0530

    Update AvailableMeals.js

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -2,51 +2,6 @@ import Card from '../UI/Cart'
 import classes from './AvailableMeals.module.css'
 import MealsItems from './MealsItems/MealsItems'
 
-// const DUMMAY_MEALS = [
-//     {
-//         Id:'m1',
-//         name:'Sushi',
-//         Desription:'Finnest fish and vaggies',
-//         price:25
-//     },
-//     {
-//         Id:'m2',
-//         name:'Veg',
-//         Desription:'Veg Food',
-//         price:20
-//     },
-//     {
-//         Id:'m3',
-//         name:'Burger',
-//         Desription:'Finnest Burger',
-//         price:10
-//     },
-//     {
-//         Id:'m3',
-//         name:'Pizza',
-//         Desription:'vag pizza',
-//         price:30
-//     }
-// ]
-
-
-
-// const AvailableMeals = ()=>{
-// const CartItems = DUMMAY_MEALS.map(meal => <MealsItems key={meal.Id} name={meal.name} Desription={meal.Desription} price={meal.price}/>)
-
-// return (
-// <section className={classes.meals}> 
-// <Card>
-// <ul>
-//         {CartItems}
-// </ul>
-// </Card>
-    
-// </section>
-// )
-// }
-// export default AvailableMeals;
-
 const DUMMY_MEALS = [
     {
         id: 'm1',
