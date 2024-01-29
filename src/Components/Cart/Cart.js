bffa1bf8372dd3262e401cc61c0e9323c60c6596
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-01-29 18:16:28 +0530

    Update Cart.js

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,21 +1,23 @@
-import classes from './Cart.module.css'
 
+import classes from './Cart.module.css'
+import Model from '../UI/Model'
 const Cart = props =>{
 const CartItems = <ul>{
 [{id:'c1', name:'Sushi', amount:2,price:12.99}
 ].map((item) => <li>{item.name}</li>)}</ul>
 
-return <div>
+return <Model onClose={props.onClose}>
     {CartItems}
     <div className={classes.total}> 
         <span>Total Amount</span>
         <span>35</span>
     </div>
     <div className={classes.actions}>
-     <button className={classes['button--alt']}></button>
+     <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
      <button className={classes.button}>Order</button>
     
     </div>
-</div>
+</Model>
 }
 export default Cart;
+
