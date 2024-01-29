00717c9ff465fa936b890e674a9381535201c6e4
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-01-29 18:17:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, {Fragment} from 'react'
+import  {Fragment, useState} from 'react'
 import Header from './Components/Layout/Header'
 import Meals from './Components/Meals/Meals';
-
+import Cart from './Components/Cart/Cart';
 
 function App() {
-  return <Fragment>
-    <Header />
+  const [cartIsShown, setCartIsShown] =  useState(false)
+  
+  const showCartHandler = () =>{
+    setCartIsShown(true)
+  }
 
+  const hideCartHandler = ()=>{
+    setCartIsShown(false)
+  }
+  return (
+  <Fragment>
+   {cartIsShown && <Cart onClose={hideCartHandler} />}
+    <Header onShowCart={showCartHandler}/>
+    
     <main>
+    
      <Meals />
+     
     </main>
   </Fragment>
    
-
+  )
 }
 
 export default App;
